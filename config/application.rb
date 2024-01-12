8a7b8e0c20b2254bb4e2a385023aa4c54df11a70
csmanel <csmanel@gmail.com>
2024-01-12 17:06:32 -0500

    application controller + application.rb

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,14 @@ module FspSpotify
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies 
+    config.middleware.use ActionDispatch::Session::Cookiestore,
+      key: '_auth_me_session',
+      same_site: :lax, 
+      secure: Rails.env.production?
+
+    Jbuilder.key_format camelize: :lower
+    Jbuilder.deep_format_keys true 
   end
 end
