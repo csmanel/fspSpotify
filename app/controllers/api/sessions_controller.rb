8ef89a760c49005dd8962c4f3b3df00fda44699b
csmanel <csmanel@gmail.com>
2024-01-15 13:21:40 -0500

    finish user auth, start fixing bug store not on window

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,30 +3,29 @@ class Api::SessionsController < ApplicationController
   before_action :require_logged_in, only: [:destroy]
   
   def show
-    @user = current_user
-    if @user
-        debugger
-        render 'api/users/show'
+    if current_user
+      @user = current_user
+      render 'api/users/show'
     else
-        render json: { user: nil }
+      render json: { user: nil }
     end
   end
 
   def create
-      username = params[:username]
-      password = params[:password]
-      @user = User.find_by_credentials(username, password)
-      if @user
-          login(@user)
-          render 'api/users/show'
-      else
-          render json: { errors: ['Invalid credentials'] }, status: 422
-      end
+    @user = User.find_by_credentials(params[:credential], params[:password])
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: { errors: ['The provided credentials were invalid.'] }, 
+        status: :unauthorized
+    end
   end
 
   def destroy
-      logout
-      head :no_content
+    logout!
+    render json: { message: 'success' }
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,38 +1,40 @@
-class User < ApplcationRecord 
+class User < ApplicationRecord
   has_secure_password
+
+  validates :username, 
+    uniqueness: true, 
+    length: { in: 3..40 }, 
+    format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..100 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..40 }, allow_nil: true
+  
   before_validation :ensure_session_token
 
-  validates :username, uniqueness: true 
-  validates :email, uniqueness: true 
-  validates :password, length: { minimum: 6 }, allow_nil: true
-  validates :session_token, presence: true, uniqueness: true
+  def self.find_by_credentials(credential, password)
+    field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :username
+    user = User.find_by(field => credential)
+    user&.authenticate(password)
+  end
 
-  def self.find_by_credentials(username, password)
-    user = User.find_by(username: username)
-    if user&.authenticate(password) 
-        return user
-    else
-        nil 
+  def reset_session_token!
+    self.update!(session_token: generate_unique_session_token)
+    self.session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    loop do
+      token = SecureRandom.base64
+      break token unless User.exists?(session_token: token)
     end
   end
 
   def ensure_session_token
     self.session_token ||= generate_unique_session_token
   end
-
-  def reset_session_token!
-    self.session_token = generate_unique_session_token
-    save!
-    session_token
-  end
-
-  private
-
-  def generate_unique_session_token
-    while true
-        token = SecureRandom.urlsafe_base64
-        return token unless User.exists?(session_token: token)
-    end
-  end
-
 end
