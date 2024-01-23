764a34e8eb910deef5a84161ff6454a9d083ccfb
csmanel <csmanel@gmail.com>
2024-01-23 09:40:22 -0500

    remove not used code

--- frontend/src/components/audio/AudioPlayer.jsx
+++ frontend/src/components/audio/AudioPlayer.jsx
@@ -1,72 +1,11 @@
-import { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  togglePlayPause,
-  playNext,
-  playPrevious,
-  setVolume,
-  setCurrentSong,
-} from '../../store/audio/audioActions';
+import { useRef, useState } from 'react';
 import PlayerControls from './PlayerControls';
 import SongDisplay from './SongDisplay';
-import ProgressBar from './ProgressBar';
-import { songs } from '../../data/songs';
-import test_src from '../../data/ahhhh.wav';
 
 export default function AudioPlayer() {
-  const dispatch = useDispatch();
-
-  // const currentSong = useSelector((state) => state.audio.currentSong);
   const [currentSong, setCurrentSong] = useState({});
-  const toggleIsPlaying = useSelector((state) => state.audio.toggleIsPlaying);
-  const volume = useSelector((state) => state.audio.volume);
-
-  const [timeProgress, setTimeProgress] = useState(0);
-  const [duration, setDuration] = useState(0);
 
   const audioRef = useRef();
-  const progressBarRef = useRef();
-
-  // useEffect(() => {
-  //   if (currentSong) {
-  // setDuration(currentSong.duration);
-  // setTimeProgress(0);
-  //     audioRef.current.src = currentSong.audioUrl;
-  //   }
-  // }, [currentSong]);
-
-  const handleNext = () => {
-    dispatch(playNext());
-  };
-
-  const handlePrevious = () => {
-    dispatch(playPrevious());
-  };
-
-  const handleSetVolume = (newVolume) => {
-    dispatch(setVolume(newVolume));
-  };
-
-  const handlePlaybackEnded = () => {
-    dispatch(playNext());
-  };
-
-  const handleTimeUpdate = () => {
-    const currentTime = audioRef.current.currentTime;
-    setTimeProgress(currentTime);
-  };
-
-  const handleSeek = (newTime) => {
-    audioRef.current.currentTime = newTime;
-  };
-
-  const handlePlayPause = () => {
-    dispatch(togglePlayPause());
-  };
-
-  const handleSongClick = (clickedSong) => {
-    dispatch(setCurrentSong(clickedSong));
-  };
 
   return (
     <div className="audio-player">
